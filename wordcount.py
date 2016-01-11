3a87e38f37a9448354e813005f635d4c8f5c71d1
Hackbright Student <info@hackbrightacademy.com>
2016-01-11 12:59:23 -0800

    improve output to be caseinsensitive and discard punctuation

--- wordcount.py
+++ wordcount.py
@@ -17,10 +17,13 @@ def count_words(file_name):
         words = line.split()
 
         for word in words:
+            word = word.lower()
+            if not word.isalpha():
+                word = word[:-1]
             quantity = word_occurance.get(word, 0)
             word_occurance[word] = quantity + 1    
 
     print word_occurance.items()
 
 
-count_words("twain.txt")    
+count_words("test.txt")    
